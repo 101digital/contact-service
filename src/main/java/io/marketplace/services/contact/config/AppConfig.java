bb83ead4818936b348c1e5060415a84aea271cfe
akilawicky <71306726+akilawicky@users.noreply.github.com>
2022-03-11 13:50:27 +0530

    config added

--- src/main/java/io/marketplace/services/contact/config/AppConfig.java
+++ src/main/java/io/marketplace/services/contact/config/AppConfig.java
@@ -3,15 +3,25 @@ package io.marketplace.services.contact.config;
 import io.micrometer.prometheus.PrometheusMeterRegistry;
 import io.prometheus.client.hotspot.GarbageCollectorExports;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
 @Configuration
+@ComponentScan({"io.marketplace.commons.gson",
+        "io.marketplace.services.pxchange.client"})
 @Import({
+        io.marketplace.commons.config.BaseSerializationConfig.class,
         io.marketplace.commons.config.GsonConfiguration.class,
         io.marketplace.commons.config.RestTemplateConfig.class,
         io.marketplace.commons.config.HttpClientConfig.class,
-        io.marketplace.commons.config.ConversionServiceConfiguration.class
+        io.marketplace.commons.config.ConversionServiceConfiguration.class,
+        io.marketplace.commons.crypto.jws.JWSUtils.class,
+        io.marketplace.commons.config.JWTFactoryConfig.class,
+        io.marketplace.commons.config.JWTConfigurations.class,
+        io.marketplace.commons.filter.CustomWebSecurity.class,
+        io.marketplace.commons.security.CustomSecurityAspect.class,
+        io.marketplace.commons.config.RequestLogConfig.class
 })
 public class AppConfig {
 
