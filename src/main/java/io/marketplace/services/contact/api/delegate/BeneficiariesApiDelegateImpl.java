d37c04829b762feae385cd85110fff5b979e31e9
TuanVu <robin.github@gmail.com>
2022-03-22 11:03:07 +0800

    add more test account

--- src/main/java/io/marketplace/services/contact/api/delegate/BeneficiariesApiDelegateImpl.java
+++ src/main/java/io/marketplace/services/contact/api/delegate/BeneficiariesApiDelegateImpl.java
@@ -10,16 +10,21 @@ import java.util.UUID;
 
 @Service
 public class BeneficiariesApiDelegateImpl implements BeneficiariesApiDelegate {
-
+    private static final String TEST_ACCOUNT_NUMBER = "BPI00000001";
 
     @Override
     public ResponseEntity<BeneficiaryAccountResponse> lookupBeneficiaryAccount(String accountNumber) {
+        String accountId = "4f95373a-6326-4b6c-85e4-d693f4befddd";
+        if (TEST_ACCOUNT_NUMBER.equals(accountNumber)) {
+            accountId = "b4ed8583-db7e-3313-afdd-de5dcb288d5c";
+        }
+
         return ResponseEntity.ok(BeneficiaryAccountResponse
                 .builder()
                 .data(BeneficiaryAccount
                         .builder()
-                        .accountId(UUID.fromString("4f95373a-6326-4b6c-85e4-d693f4befddd"))
-                        .accountNumber("120321456")
+                        .accountId(UUID.fromString(accountId))
+                        .accountNumber(TEST_ACCOUNT_NUMBER)
                         .displayName("John Does")
                         .build())
                 .build());
@@ -27,12 +32,16 @@ public class BeneficiariesApiDelegateImpl implements BeneficiariesApiDelegate {
 
     @Override
     public ResponseEntity<BeneficiaryAccountResponse> lookupBeneficiaryAccountWithMobile(String mobileNumber) {
+        String accountId = "4f95373a-6326-4b6c-85e4-d693f4befddd";
+        if ("84972966263".equals(mobileNumber)) {
+            accountId = "b4ed8583-db7e-3313-afdd-de5dcb288d5c";
+        }
         return ResponseEntity.ok(BeneficiaryAccountResponse
                 .builder()
                 .data(BeneficiaryAccount
                         .builder()
-                        .accountId(UUID.fromString("4f95373a-6326-4b6c-85e4-d693f4befddd"))
-                        .accountNumber("120321456")
+                        .accountId(UUID.fromString(accountId))
+                        .accountNumber(TEST_ACCOUNT_NUMBER)
                         .displayName("John Does")
                         .build())
                 .build());
