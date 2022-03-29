879f8447c2c4ce64c24360728f9b3bb14d87415c
akilawicky <71306726+akilawicky@users.noreply.github.com>
2022-03-29 13:54:30 +0530

    Kafka config fix

--- src/main/java/io/marketplace/services/contact/config/KafkaReceiverConfig.java
+++ src/main/java/io/marketplace/services/contact/config/KafkaReceiverConfig.java
@@ -1,11 +1,8 @@
 package io.marketplace.services.contact.config;
 
-import org.apache.kafka.clients.admin.AdminClientConfig;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
@@ -14,31 +11,23 @@ import org.springframework.kafka.config.KafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
 import org.springframework.kafka.listener.ConcurrentMessageListenerContainer;
-import org.springframework.kafka.listener.ContainerProperties;
-import org.springframework.util.StringUtils;
 
+import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 @EnableKafka
 @Configuration
 public class KafkaReceiverConfig {
 
-    private final ExtraConfigLoader configLoader;
-    private final KafkaProperties kafkaProperties;
+    @Value("${kafka.server}")
+    private String bootstrapServers;
 
-    @Autowired
-    public KafkaReceiverConfig(ExtraConfigLoader appProps, KafkaProperties kafkaProperties) {
-        this.configLoader = appProps;
-        this.kafkaProperties = kafkaProperties;
-    }
-
-    @Value("${kafka.consumer.groupid}")
+    @Value("${kafka.groupid:wallet-processing-data-group}")
     private String consumerGroupId;
 
-    @Value("${kafka.consumer.maxPollRecords:1000}")
-    private int maxPollRecords;
-
-    @Value("${kafka.consumer.numberProcessor:1}")
+    @Value("${kafka.consumer.number-processor:2}")
     private int numberProcessor;
 
     private String KAFKA_TIMEOUT = "60000";
@@ -46,24 +35,16 @@ public class KafkaReceiverConfig {
     private String HEARTBEAT_INTERVAL_MS_CONFIG = "20000";
 
     @Bean
-    @SuppressWarnings("Duplicates")
     public Map<String, Object> consumerConfigs() {
-        
-        Map<String, Object> props = kafkaProperties.buildConsumerProperties();
+        Map<String, Object> props = new HashMap<>();
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
         props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         props.put(ConsumerConfig.GROUP_ID_CONFIG, consumerGroupId);
         props.put(ConsumerConfig.SESSION_TIMEOUT_MS_CONFIG, KAFKA_TIMEOUT);
-        props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, false);
+        props.put(ConsumerConfig.AUTO_COMMIT_INTERVAL_MS_CONFIG, KAFKA_COMMIT_INTERVAL);
         props.put(ConsumerConfig.HEARTBEAT_INTERVAL_MS_CONFIG, HEARTBEAT_INTERVAL_MS_CONFIG);
         props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
-        props.put(ConsumerConfig.MAX_POLL_RECORDS_CONFIG, maxPollRecords);
-
-        String jaasConfig = configLoader.getJaasConfig();
-        if (!StringUtils.isEmpty(jaasConfig)) {
-            props.put(AdminClientConfig.SECURITY_PROTOCOL_CONFIG, "SASL_PLAINTEXT");
-            props.put("sasl.jaas.config", jaasConfig);
-        }
         return props;
     }
 
@@ -77,8 +58,12 @@ public class KafkaReceiverConfig {
         ConcurrentKafkaListenerContainerFactory<String, String> factory = new ConcurrentKafkaListenerContainerFactory<>();
         factory.setConsumerFactory(consumerFactory());
         factory.setConcurrency(numberProcessor);
-        factory.getContainerProperties().setAckMode(ContainerProperties.AckMode.MANUAL_IMMEDIATE);
-
         return factory;
     }
+
+    @Bean
+    public ExecutorService executorService() {
+        return Executors.newFixedThreadPool(numberProcessor);
+    }
+
 }
